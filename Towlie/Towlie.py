9c9958891a959c790ac2b8525561b07e720603ca
cchave17 <carloschavezdeveloper@gmail.com>
2023-09-16 00:46:21 -0500

    fixed leaked token

--- Towlie/Towlie.py
+++ Towlie/Towlie.py
@@ -1,55 +1,55 @@
-# import discord
-# import os
-# from dotenv import load_dotenv
-# from discord.ext import commands
-# from cogs.reaction_roles import ReactionRoles
-# from cogs.ticket_system import TicketSystem
-# from cogs.gpt_prompt import GptPrompt
-
-# # Load environment variables from .env file
-# load_dotenv()
-
-# intents = discord.Intents.all()
-
-# bot = commands.Bot(command_prefix='!', intents=intents)
-
-# print(os.environ)
-
-# @bot.event
-# async def on_ready():
-#     print(f'{bot.user} is connected to Discord!')
-#     await bot.add_cog(ReactionRoles(bot))
-#     await bot.add_cog(TicketSystem(bot))
-#     await bot.add_cog(GptPrompt(bot))
-
-# bot.run(os.getenv('TOWLIE_TOKEN'))
-
 import discord
-from googletrans import Translator
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+from cogs.reaction_roles import ReactionRoles
+from cogs.ticket_system import TicketSystem
+from cogs.gpt_prompt import GptPrompt
+
+# Load environment variables from .env file
+load_dotenv()
 
 intents = discord.Intents.all()
 
-# Initialize the bot
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-translator = Translator()
+print(os.environ)
 
-@client.event
+@bot.event
 async def on_ready():
-    print("Bot is ready")
+    print(f'{bot.user} is connected to Discord!')
+    await bot.add_cog(ReactionRoles(bot))
+    await bot.add_cog(TicketSystem(bot))
+    await bot.add_cog(GptPrompt(bot))
 
-@client.event
-async def on_message(message):
-    # Don't reply to ourselves
-    if message.author == client.user:
-        return
+bot.run(os.getenv('TOWLIE_TOKEN'))
 
-    if message.content.startswith('!translate'):
-        _, src, dest, *text = message.content.split(" ")
-        text = ' '.join(text)
-        translation = translator.translate(text, src=src, dest=dest)
-        print(translation)
-        await message.channel.send(translation.text)
+# import discord
+# from googletrans import Translator
 
-# Run the bot with your token
-client.run('MTEzNDcxMTg4NDk4NjE4Nzg0OQ.Gyslce.HXivzsoiG-r26__VBEklPrrRSmxVVAU2SGFRrU')
+# intents = discord.Intents.all()
+
+# # Initialize the bot
+# client = discord.Client(intents=intents)
+
+# translator = Translator()
+
+# @client.event
+# async def on_ready():
+#     print("Bot is ready")
+
+# @client.event
+# async def on_message(message):
+#     # Don't reply to ourselves
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('!translate'):
+#         _, src, dest, *text = message.content.split(" ")
+#         text = ' '.join(text)
+#         translation = translator.translate(text, src=src, dest=dest)
+#         print(translation)
+#         await message.channel.send(translation.text)
+
+# # Run the bot with your token
+# client.run('token_dont_show')
